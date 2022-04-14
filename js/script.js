87eff12c5204c516517ea3cce842789ae1661502
NastyaSh <shulhaevich@gmail.com>
2022-04-14 16:15:02 +0300

    second JS practice

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 "use sctrict"
-const numberOfFilms = +prompt("Сколько фильмов вы посмотрели?", "");
+/*const numberOfFilms = +prompt("Сколько фильмов вы посмотрели?", "");
 
 const personalMoviesDB = {
 	count: numberOfFilms,
@@ -8,6 +8,7 @@ const personalMoviesDB = {
 	genres: [],
 	privat: false
 };
+
 
 const a = prompt("Один из последний просмотреных фильмов?", "");
 const b = prompt("На сколько его оцените?", "");
@@ -18,3 +19,43 @@ personalMoviesDB.movies[a] = b;
 personalMoviesDB.movies[c] = d;
 
 console.log(personalMoviesDB);
+*/
+
+const numberOfFilms = +prompt("Сколько фильмов вы посмотрели?", "");
+
+const personalMoviesDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+for (let i = 0; i < 2; i++) {
+	const a = prompt("Один из последний просмотреных фильмов?", ""),
+		b = prompt("На сколько его оцените?", "");
+
+	if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+		personalMoviesDB.movies[a] = b;
+		console.log("done");
+	} else {
+		console.log("error");
+		i--;
+	}
+}
+
+if (personalMoviesDB.count < 10) {
+	console.log("Просмотрено довольно мало фильмов");
+} else if (personalMoviesDB.count >= 10 && personalMoviesDB.count < 30) {
+	console.log("Вы класический зритель");
+} else if (personalMoviesDB.count >= 30) {
+	console.log("Вы киноман");
+} else {
+	console.log("Произошла ошибка");
+}
+
+
+
+console.log(personalMoviesDB);
+
+
